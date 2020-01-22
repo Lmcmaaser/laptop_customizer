d26024a315ce60a055276d8fb1e9110a57853465
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-01-22 13:07:52 -0700

    created Total component

--- src/Summary.js
+++ src/Summary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { USCurrencyFormat } from './USCurrencyFormat'
+import Total from './Total.js'
 import './Summary.css'
 
 class Summary extends React.Component {
@@ -18,24 +19,15 @@ class Summary extends React.Component {
             ); 
         });
 
-        const total = Object.keys(this.props.selected).reduce(
-            (acc, curr) => acc + this.props.selected[curr].cost,
-            0
-        );
-
         return (
             <>
                 <section className="main__summary">
                     <h2>Your cart</h2>
                     {summary}
-                    <div className="summary__total">
-                            <div className="summary__total__label">
-                                Total
-                            </div>
-                            <div className="summary__total__value">
-                                {USCurrencyFormat.format(total)}
-                            </div>
-                        </div>
+                    <Total
+                        selected={this.state.selected}
+                        total={this.props.summary}
+                    />
                 </section>
             </>
         )
